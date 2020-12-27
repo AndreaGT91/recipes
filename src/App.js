027e0bec488d135c2ea97b44916fa7e7f5c5b466
Andrea Bentley <andrea@bentleytech.com>
2020-12-26 20:05:40 -0500

    Added final code to display recipe list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
+import React, { useEffect } from "react";
+import { Container, Row, Col, Card, CardColumns } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import recipeList from "./recipes.json";
 
 function App() {
+
+  useEffect(() => {
+    recipeList.sort(function(a, b) {
+      if (a.sortname < b.sortname) return -1
+      else if (a.sortname > b.sortname) return 1
+      else return 0;
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className="d-block mx-auto text-center bg-light">
+      <Row>
+        <Col>
+          <h1 className="text-center p-4">Bentley Family Favorite Recipes</h1>
+        </Col>
+      </Row>
+      <CardColumns>
+        {recipeList.map((recipe, index) => (
+          <Card key={index} className="p-3" bg="dark">
+            <Card.Link href={`http://bentleytech.com/recipes/${recipe.filename}`} 
+              target="_blank" rel="noopener noreferrer" style={{ color:"white" }}>
+              <Card.Title className="font-weight-bold">{recipe.title}</Card.Title>
+              <Card.Text className="font-weight-lighter">{recipe.source}</Card.Text>
+            </Card.Link>
+          </Card>
+        ))}
+      </CardColumns>
+    </Container>
   );
-}
+};
 
 export default App;
